98fd72281eec53c32441acc5d179c9a87cae8b1a
GretkaMilkovic <mybeachaddiction@gmail.com>
2020-12-06 13:00:55 +0100

    Updated HTML and CSS

--- src/script.js
+++ src/script.js
@@ -79,7 +79,4 @@ dateElement.innerHTML = formatDate(currentTime);
 let searchForm = document.querySelector("#search-form");
 searchForm.addEventListener("submit", handleSubmit);
 
-let currentLocationButton = document.querySelector("#current-location-button");
-currentLocationButton.addEventListener("click", getCurrentLocation);
-
 searchCity("Munich");
