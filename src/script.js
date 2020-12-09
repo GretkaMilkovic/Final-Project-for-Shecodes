45f337b2b972e58d815ae1fb1f14783505af5aad
GretkaMilkovic <mybeachaddiction@gmail.com>
2020-12-09 13:43:33 +0100

    showing the current data and temperature

--- src/script.js
+++ src/script.js
@@ -1,94 +1,27 @@
 
-function formatDate(date) {
-  let hours = date.getHours();
-  if (hours < 10) {
-    hours = `0${hours}`;
-  }
-  let minutes = date.getMinutes();
-  if (minutes < 10) {
-    minutes = `0${minutes}`;
-  }
-
-  let dayIndex = date.getDay();
-  let days = [
-    "Sun",
-    "Mon",
-    "Tue",
-    "Wed",
-    "Thur",
-    "Fri",
-    "Sat",
-  ];
-  let day = days[dayIndex];
-
-  return ` ${hours}:${minutes}, ${day}`;
-}
-
-function displayWeatherCondition(response) {
-  document.querySelector("#city").innerHTML = response.data.name;
-  document.querySelector("#temperature").innerHTML = Math.round(
-    response.data.main.temp
-  );
-
-  document.querySelector("#humidity").innerHTML = response.data.main.humidity;
 
 
-  document.querySelector("#pressure").innerHTML = Math.round(
-    response.data.main.pressure
-  );
-  document.querySelector("#wind").innerHTML = Math.round(
-    response.data.wind.speed
-  );
 
+function displayTemperature(response) {
+  console.log(response.data.city);
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
 
+  windElement.innerHTML = Math.round(response.data.main.wind);
+  humidityElement.innerHTML = Math.round(response.data.main.humidity);
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+
+  cityElement.innerHTML = response.data.name;
   
-  document.querySelector("#description").innerHTML =
-    response.data.weather[0].main;
+  descriptionElement.innerHTML = response.data.weather[0].description;
 }
 
-function searchCity(city) {
   let apiKey = "82b95efea5e472e6661eb0b2204689f3";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-  axios.get(apiUrl).then(displayWeatherCondition);
-}
-console.log(apiUrl);
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayTemperature);
 
 
-function searchLocation(position) {
-  let apiKey = "82b95efea5e472e6661eb0b2204689f3";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
 
-  axios.get(apiUrl).then(displayWeatherCondition);
-}
-
-function getCurrentLocation(event) {
-  event.preventDefault();
-  navigator.geolocation.getCurrentPosition(searchLocation);
-}
-
-function handleSubmit(event) {
-  event.preventDefault();
-  let city = document.querySelector("#city-input").value;
-  searchCity(city);
-}
-
-function convertToFahrenheit(event) {
-  event.preventDefault();
-  let temperatureElement = document.querySelector("#temperature");
-  temperatureElement.innerHTML = 66;
-}
-
-function convertToCelsius(event) {
-  event.preventDefault();
-  let temperatureElement = document.querySelector("#temperature");
-  temperatureElement.innerHTML = 2;
-}
-
-let dateElement = document.querySelector("#date");
-let currentTime = new Date();
-dateElement.innerHTML = formatDate(currentTime);
-
-let searchForm = document.querySelector("#search-form");
-searchForm.addEventListener("submit", handleSubmit);
-
-searchCity("Munich");
